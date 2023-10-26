acc394b64b2f3dfb933e747d523b531b92ea8583
EC2 Default User <ec2-user@ip-172-31-47-250.us-west-2.compute.internal>
2023-10-26 20:00:30 +0000

    Initial commit

---
+++ test/paco-crud-id.app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PacoCrudIdApp from '../lib/paco-crud-id.app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/paco-crud-id.app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PacoCrudIdApp.PacoCrudIdAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
